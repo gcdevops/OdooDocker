06aa16e574dca5d6b4afc5e8819efaa6c3ad3c6b
Moro-Code <omardev9898@gmail.com>
2020-03-28 15:48:15 -0400

    employees custom module override

---
+++ add-ons/hr_modifier/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Employees Modifier',
+    'version': '0.1',
+    'category': 'Human Resources/Employees',
+    'summary': 'Cutomize Employee Module for our purposes',
+    
+    'depends': [
+        'hr'
+    ],
+    'data': [
+        'views/hr_plan_views.xml',
+        'views/hr_employee_category_views.xml',
+        'views/hr_employee_public_views.xml',
+        'views/hr_employee_views.xml',
+        'views/hr_department_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
