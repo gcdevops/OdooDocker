1fa9c8bfa0bd24b387ce29d515ac594e267a1b21
Moro-Code <omardev9898@gmail.com>
2020-04-02 21:28:26 -0400

    adding custom fields

--- add-ons/hr_modifier/__manifest__.py
+++ add-ons/hr_modifier/__manifest__.py
@@ -11,12 +11,15 @@
         'base', 'hr'
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/hr_job_views.xml',
         'views/hr_plan_views.xml',
         'views/hr_employee_category_views.xml',
         'views/hr_employee_public_views.xml',
         'views/hr_employee_views.xml',
         'views/hr_department_views.xml',
+        'views/hr_employee_classification_views.xml',
+        'views/hr_views.xml'
     ],
     'installable': True,
     'application': False,
--- add-ons/hr_modifier/models/__init__.py
+++ add-ons/hr_modifier/models/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import hr_department
+from . import hr_classification
+from . import hr_employee_base
+from . import hr_employee
+from . import hr_employee_public
