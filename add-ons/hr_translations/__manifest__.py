69fd9139dd46f0084c155c70cc73da794b98b369
Moro-Code <omardev9898@gmail.com>
2020-04-07 17:09:25 -0400

    generated translation values and module

---
+++ add-ons/hr_translations/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Employees Translations",
+    "version": "0.1",
+    "category": "Human Resources/Employees",
+    "summary": "Employee Translations",
+    "depends": [
+        "base", "hr_modifier"
+    ],
+    "installable": True,
+    "application": False
+}
