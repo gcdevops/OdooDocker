e41b3e98946a46235b17b4aed8d1e6fbd3119af0
Gabriel Cossette <gabriel.cossette@hrsdc-rhdcc.gc.ca>
2020-04-07 10:49:59 -0400

    remove level criticality

--- add-ons/hr_modifier/models/hr_employee_base.py
+++ add-ons/hr_modifier/models/hr_employee_base.py
@@ -5,14 +5,7 @@ class HrEmployeeBase(models.AbstractModel):
     _inherit = "hr.employee.base"
 
     x_employee_work_criticality = fields.Boolean("Work criticality")
-    x_employee_level_criticality = fields.Selection(
-        [
-            ("1", "1 - Call centres"),
-            ("2", "2 - Claims processing"),
-            ("3", "3 - Others")
-        ],
-        string = "Level of criticality"
-    )
+
     x_employee_language = fields.Selection(
         [
             ("english", "English"),
