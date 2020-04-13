6a3a8c7de4d5c51c42d0815296936e10fe56de5f
Moro-Code <omardev9898@gmail.com>
2020-04-13 14:35:57 -0400

    switched fields to public view

--- add-ons/hr_modifier/models/hr_employee_public.py
+++ add-ons/hr_modifier/models/hr_employee_public.py
@@ -3,6 +3,37 @@ from odoo import fields, models
 class HrEmployeePublic(models.Model):
     _name= "hr.employee.public"
     _inherit = "hr.employee.public"
+    x_employee_language = fields.Selection(readonly = True)
+
+    x_employee_job_type = fields.Char(readonly = True)
+    
+    x_employee_status = fields.Selection(readonly = True)
+
+    x_employee_access_gov_office = fields.Boolean(readonly=True)
+    
+    x_employee_device_type = fields.Selection(readonly=True)
+
+    x_employee_asset_number = fields.Char(readonly=True)
+    
+    x_employee_office_floor = fields.Char(readonly=True)
+    
+    x_employee_office_cubicle = fields.Char(readonly=True)
+
+    x_employee_is_remote = fields.Boolean(readonly=True)
+
+    x_employee_second_monitor = fields.Boolean(readonly=True)
+
+    x_employee_mobile_hotspot = fields.Boolean(readonly=True)
+
+    x_employee_headset = fields.Boolean(readonly=True)
+
+    classification_id = fields.Many2one(readonly=True)
+
+    region_id = fields.Many2one(readonly=True)
+
+    x_employee_remote_access_network = fields.Boolean(readonly=True)
+
+    x_employee_remote_access_tool = fields.Selection(readonly = True)
 
     parent_id = fields.Many2one(
         'hr.employee.public', 
