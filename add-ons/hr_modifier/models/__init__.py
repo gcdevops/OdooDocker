df914f9474def4595dc99613b3f220bc5d092a41
Gabriel Cossette <gabriel.cossette@hrsdc-rhdcc.gc.ca>
2020-04-24 17:09:18 -0400

    rename to Skills to Profiles + reorg menu

--- add-ons/hr_modifier/models/__init__.py
+++ add-ons/hr_modifier/models/__init__.py
@@ -5,5 +5,7 @@ from . import hr_employee_base
 from . import hr_employee
 from . import hr_employee_public
 from . import hr_region
+from . import hr_resume
+from . import hr_skills
 from . import res_users
 from . import res_partner
