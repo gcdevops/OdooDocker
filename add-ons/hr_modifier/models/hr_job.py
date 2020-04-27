c01cc9c49ad4fd9d4c28f052a4d57ce061e48d4c
kyle <kyle.meelker@gmail.com>
2020-04-27 16:04:37 -0400

    fixed up job title validation

--- add-ons/hr_modifier/models/hr_job.py
+++ add-ons/hr_modifier/models/hr_job.py
@@ -4,10 +4,8 @@ import re
 
 
 class HrJob(models.Model):
-    #_name = "hr.job"
+    _name = "hr.job"
     _inherit = "hr.job"
-
-    name = fields.Char(string="Region Name", required = True)
 
     #  Validation
 
@@ -15,8 +13,5 @@ class HrJob(models.Model):
     @api.constrains("name")
     def _check_alpha_numeric_space_dash(self):
         for record in self:
-            res = re.match("[^[\w -]+$", record.name)
-            if res:
-                raise ValidationError("The job position can only contain letters, numbers, spaces, and hyphens: ", res)
-            else:
-                raise ValidationError("The job title matches: ", res)
+            if re.search("[^a-zA-Z\d\s:\-,]", record.name):
+                raise ValidationError("The job position can only contain letters, numbers, spaces, commas, and hyphens")
