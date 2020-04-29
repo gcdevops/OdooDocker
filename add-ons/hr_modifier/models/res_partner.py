1dbc52776e495229894525f5cddae47a8ab6a10d
kyle <kyle.meelker@gmail.com>
2020-04-29 11:30:16 -0400

    added more validation to location views

--- add-ons/hr_modifier/models/res_partner.py
+++ add-ons/hr_modifier/models/res_partner.py
@@ -8,6 +8,14 @@ class Partner(models.Model):
 
     # Validation
 
+    # allow upper case, lower case, hyphens
+    @api.constrains("name")
+    def _check_name_allowed_characters(self):
+        for record in self:
+            if record.name:
+                if not re.search("[a-zA-Z\s\- \.]", record.name):
+                    raise ValidationError("The location name can only contain letters, spaces, and hyphens")
+
     # allow alphanumeric, spaces, and hyphens
     @api.constrains("street", "street2")
     def _check_street_allowed_characters(self):
@@ -35,4 +43,32 @@ class Partner(models.Model):
             if record.zip:
                 if not re.search("^([a-zA-Z]\d[a-zA-Z][ ]?\d[a-zA-Z]\d)$", record.zip):
                     raise ValidationError("The postal code is invalid")
-                    
+    
+    @api.constrains("phone", "mobile")
+    def _check_phone_allowed_characters(self):
+        for record in self:
+            phone_disallowed_chars = "[^\d\-]"
+            if record.phone:
+                if re.search(phone_disallowed_chars, record.phone):
+                    raise ValidationError("The phone number can only contain numbers, hyphens")
+            if record.mobile:
+                if re.search(phone_disallowed_chars, record.mobile):
+                    raise ValidationError("The mobile phone number can only contain numbers, hyphens")
+
+    # if special characters are found, raise an error
+    @api.constrains("email")
+    def _check_email_allowed_characters(self):
+        for record in self:
+            if record.email:
+                res = re.search(r"['\"*?;/\\]", record.email)
+                if res:
+                    raise ValidationError("The email contains an invalid character: " + res.group(0))
+
+    # if special characters are found, raise an error
+    @api.constrains("website")
+    def _check_website_allowed_characters(self):
+        for record in self:
+            if record.website:
+                res = re.search(r"[^\w\d\-.~:/?#\[\]@!$&'()*+,;=]", record.website)
+                if res:
+                    raise ValidationError("The website url contains an invalid character: " + res.group(0))
