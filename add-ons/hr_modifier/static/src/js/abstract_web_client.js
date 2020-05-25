629428d75ecbf9f14f1209600ae21c419b6f760d
Gabriel Cossette <gabriel.cossette@hrsdc-rhdcc.gc.ca>
2020-05-25 13:18:42 -0400

    fix typo

--- add-ons/hr_modifier/static/src/js/abstract_web_client.js
+++ add-ons/hr_modifier/static/src/js/abstract_web_client.js
@@ -7,7 +7,7 @@ odoo.define('hr_modifier.AbstractWebClient', function (require) {
 
         init: function (parent) {
             this._super(parent);
-            this.set('title_part', {"zopenerp": "Listeblanche RH | HR Whitelist"});
+            this.set('title_part', {"zopenerp": "Liste blanche RH | HR Whitelist"});
         },
 
     });
