8ef1569b0441fff2252b2829cafd4db2e9bb9c06
Kyle Meelker <kyle.meelker@gmail.com>
2020-05-25 14:07:22 -0400

    managed to get the branch name to appear

--- add-ons/hr_modifier/static/src/js/custom_title.js
+++ add-ons/hr_modifier/static/src/js/custom_title.js
@@ -1,26 +1,36 @@
-odoo.define("hr_modifier.LanguageToggle",function(require){
+odoo.define("hr_modifier.CustomTitle", function (require) {
   "use strict";
-  var ControlPanelMixin = require('web.ControlPanelMixin');
-  var Widget = require('web.Widget');
 
-  var ClientAction = Widget.extend(ControlPanelMixin, {
-    start: function() {
-      this._renderButtons();
-      this._updateControlPanel();
-    },
-    do_show: function() {
-      this._updateControlPanel();
-    },
-    _renderButtons: function() {
+  var Widget = require("web.Widget");
+  var SystrayMenu = require('web.SystrayMenu');
+  var session = require("web.session")
+  var dom = require("web.dom");
+  var rpc = require("web.rpc")
 
-    },
-    _updateControlPanel: function() {
-      this.update_control_panel({
-        cp_content: {
-          $buttons: this.buttons;
+  var CustomTitle = Widget.extend({
+    willStart: function () {
+      var self = this
+      return rpc.query({
+        model: "res.users",
+        method: "search_read",
+        args: [[["id", "=", session.uid]]]
+      }).then(function(result){
+        if (result && result.length > 0 && result[0].x_department_coordinators_ids && result[0].x_department_coordinators_ids.length > 0){
+          rpc.query({
+            model: "hr.department",
+            method: "search_read",
+            args: [[["id", "in", result[0].x_department_coordinators_ids]]]
+          }).then(function(result){
+            if (result && result.length > 0 && result[0].name){
+              dom.append($("header nav"), "<span class='o_team_name'>" + result[0].name + "</span>");
+            }            
+          })
         }
-      })
+        
+      });
     }
   });
-
-}
+  CustomTitle.prototype.sequence = -20000;
+  SystrayMenu.Items.push(CustomTitle);
+  return CustomTitle
+})
